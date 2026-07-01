830b7adb0c2b9a5637fc46ff4e5754b4943a08e4
AmandaPradoGit <amandapradogc@gmail.com>
2026-06-30 21:16:00 -0300

    feat: add LevelsPage component with introductory content

---
+++ app/levels/page.tsx
@@ -0,0 +1,13 @@
+export default function LevelsPage() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Níveis</h1>
+      <p>Bem-vindo à página de níveis do Sign Language Recognizer.</p>
+      <ul>
+        <li>Nível 1</li>
+        <li>Nível 2</li>
+        <li>Nível 3</li>
+      </ul>
+    </main>
+  );
+}
