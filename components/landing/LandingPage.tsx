92221b65cd38208941fdbb1c2d7a5115c7fe283e
AmandaPradoGit <amandapradogc@gmail.com>
2026-06-30 21:19:39 -0300

    feat: remove Header component from LandingPage

--- components/landing/LandingPage.tsx
+++ components/landing/LandingPage.tsx
@@ -1,13 +1,11 @@
-import GeometricBackground from './GeometricBackground';
-import Header from './Header';
-import HeroSection from './HeroSection';
+import GeometricBackground from './GeometricBackground'
+import HeroSection from './HeroSection'
 
 export default function LandingPage() {
   return (
     <div className="min-h-screen overflow-x-hidden bg-white">
       <GeometricBackground />
       <div className="relative z-10">
-        <Header />
         <HeroSection />
       </div>
     </div>
